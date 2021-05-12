67ab0f545ddd65c62e1b2e757384aa956518b348
Max Schmeling <max@clipisode.com>
2021-05-12 11:26:33 -0500

    Add site data and custom site css

--- src/index.worker.ts
+++ src/index.worker.ts
@@ -1,5 +1,9 @@
-import { GetCustomDataDescriptorsFn } from "@clipisode/theme";
+import { GetCustomDataDescriptorsFn, SiteData } from "@clipisode/theme";
 
 export const getCustomDataDescriptors: GetCustomDataDescriptorsFn = () => {
   return [];
+};
+
+export const siteData: SiteData = {
+  customCssKey: "site.css",
 };
